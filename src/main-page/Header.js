a5ea9253e3f38520021dc7b7c3a0dd22d1744e27
Vasyl Semak <vsemak@c11869.myfiosgateway.com>
2022-03-24 17:31:23 -0700

    create Header component

---
+++ src/main-page/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+const logo = require("./globoLogo.png");
+
+
+const Header = () => (
+    <header className="row">
+        <div className="col-md-5">
+            <img src={logo} className="logo" alt="logo" />
+        </div>
+        <div className="col-md-7 mt-5 subtitle">
+            Providing houses all over the world
+        </div>
+    </header>
+);
+
+export default Header;
