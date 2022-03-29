36e275490fc0dedc8cae037eff2a9cb935995c78
Vasyl Semak <vsemak@c11869.myfiosgateway.com>
2022-03-29 08:30:58 -0700

    fetch houses from houses.json via useEffect, establish main-page state - allhouses

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './main-page';
-import reportWebVitals from './reportWebVitals';
+import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
--- src/main-page/index.js
+++ src/main-page/index.js
@@ -1,7 +1,19 @@
 import './main-page.css';
 import Header from './Header';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [allHouses, setAllHouses] = useState([]);
+
+  useEffect(() => {
+    const fetchHouses = async () => {
+      const response = await fetch("/houses.json");
+      const houses = await response.json();
+      setAllHouses(houses)
+    }
+    fetchHouses();
+  }, []);
+
   return (
     <div className="container">
       <Header />
@@ -10,3 +22,22 @@ function App() {
 }
 
 export default App;
+
+
+
+/* {
+  allHouses.map((house) => (
+    <div key={house.id} className="row">
+      <h1 className='col-md-5'>
+        {house.price}
+      </h1>
+      <h2 className='col-md-5'>
+        {house.country}
+      </h2>
+      <p className='col-md-5'>
+        {house.address}
+      </p>
+      <img src={house.photo} alt="house"/>
+    </div>
+  ))
+} */
